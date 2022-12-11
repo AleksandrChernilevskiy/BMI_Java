37b2695e00707b7a9588a5c0fd20477c33a2d99a
Azez <aleksanderterar@gmail.com>
2022-12-11 22:37:12 +0300

    initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,14 @@
+public class BmiService {
+    public double calculate () {
+        double mansHeight = 185;
+        double mass = 65;
+        double mansHeightFormula = (double) Math.pow( (mansHeight / 100) , 2);
+        double result;
+        if ( mass > mansHeightFormula ){
+            result = (double) (mass / mansHeightFormula);
+        } else {
+            result = 0;
+        }
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double bmi = service.calculate();
+        System.out.println("Индекс массы тела " + (int)bmi);
+        System.out.println("Категория                        ИМТ");
+        System.out.println("Острый дефицит массы            < 16");
+        System.out.println("Недостаточная масса тела   16 - 18.5");
+        System.out.println("Норма                      18.6 - 25");
+        System.out.println("Избыточная масса тела      25.1 - 30");
+        System.out.println("Ожирение первой степени    30.1 - 35");
+        System.out.println("Ожирение второй степени    35.1 - 40");
+        System.out.println("Ожирение третьей степени      > 40.1");
+    }
+}
