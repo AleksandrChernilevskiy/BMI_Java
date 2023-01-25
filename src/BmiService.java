af97b89c9c38f3f92272589a1bb74e038e8f4ccb
Azez <aleksanderterar@gmail.com>
2023-01-25 23:40:57 +0300

    Change

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public double calculate () {
-        double mansHeight = 185;
-        double mass = 65;
+    public double calculate (double mansHeight, double mass) {
+        //double mansHeight = 185;
+        //double mass = 65;
         double mansHeightFormula = (double) Math.pow( (mansHeight / 100) , 2);
         double result;
         if ( mass > mansHeightFormula ){
